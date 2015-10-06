93b714bef4cbbcedbca5961aed112e44ab9e16ed
jack0088 <jack0088@me.com>
2015-10-06 16:15:38 +0200

    -deleted useless dump.lua -Groups got new method :pivotChildren() - implementing pivots and offsets for Group children right now… everything buggy ;(

--- lib/Groups.lua
+++ lib/Groups.lua
@@ -1,3 +1,44 @@
+local function draw(obj) -- recursive
+  for _, child in ipairs(obj) do
+    if child.draw then child:draw() end
+    if child.children then draw(child.children) end
+  end
+end
+
+local function update(self)
+  -- Update self.size and self.offset based on self.children
+  if not self:pivotChildren() then return end
+  
+  local list = {x = {}, y = {}}
+  local function collect(obj) -- recursive
+    for _, child in ipairs(obj) do
+      -- Gather all positions for sorting
+      local verts = {
+        child:getPivotPosition(0, 0),
+        child:getPivotPosition(0, 1),
+        child:getPivotPosition(1, 1),
+        child:getPivotPosition(1, 0)
+      }
+      for i = 1, #verts do
+        table.insert(list.x, verts[i].x)
+        table.insert(list.y, verts[i].y)
+      end
+      if child.children then collect(child.children) end
+    end
+  end
+  
+  -- Find smallest and biggest positions and update self
+  collect(self.children)
+  table.sort(list.x)
+  table.sort(list.y)
+  local min  = Vector(list.x[1], list.y[1])
+  local max  = Vector(list.x[#list.x], list.y[#list.y])
+  
+  self.offset = self:getPosition() - min
+  self:setSize((max - min):unpack())
+end
+
+
 require "lib.Classes"
 require "lib.Vectors"
 require "lib.Objects"
@@ -6,15 +47,10 @@ Group = class(Object)
 
 function Group:init(...)
   Object.init(self)
+  self.offset = Vector(0, 0)
+  self._pivotChildren = false
   self.children = {}
   self:addChild(...)
-end
-
-local function draw(children)
-  for _, child in ipairs(children) do
-    if child.draw then child:draw() end
-    if child.children then draw(child.children) end
-  end
 end
 
 function Group:draw()
@@ -23,7 +59,12 @@ function Group:draw()
   
   love.graphics.push()
   love.graphics.push()
+  love.graphics.push()
   Object.draw(self)
+  if self:pivotChildren() then
+    love.graphics.pop()
+    love.graphics.translate((self.offset):unpack())
+  end
   
   draw(self.children)
   
@@ -37,6 +78,14 @@ function Group:draw()
   love.graphics.point(x, y);
   
   love.graphics.pop()
+end
+
+function Group:pivotChildren(tag) -- set & get
+  if tag then
+    self._pivotChildren = tag
+    update(self)
+  end
+  return self._pivotChildren
 end
 
 function Group:addChild(...)
@@ -61,13 +110,16 @@ function Group:addChild(...)
       table.insert(self.children, child)
     end
   end
+  
+  if self:pivotChildren() then update(self) end
 end
 
 function Group:removeChild(obj)
   for id, child in ipairs(self.children) do
-      if child == obj then
-        table.remove(self.children, id)
-        break
-      end
+    if child == obj then
+      table.remove(self.children, id)
+      break
     end
+  end
+  if self:pivotChildren() then update(self) end
 end
